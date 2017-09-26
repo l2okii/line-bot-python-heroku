f730616a12b0eb9a2ffdebd21b45a80b3171a5ed
l2okii <sfuangk@celestica.com>
2017-09-26 15:31:13 +0700

    regis to db

--- db_adapter.py
+++ db_adapter.py
@@ -19,58 +19,87 @@ conn = psycopg2.connect(
 # )
 
 def select_test():
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
     sql = """SELECT * from l2ig_db;"""
-    # try:
-    cur = conn.cursor()
-    cur.execute(sql)
-    rows = cur.fetchall()
-    for row in rows:
-        print ' ', row
-    cur.close()
-    return rows
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print ' ', row
+        cur.close()
+        return rows
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def select_by_line(line_id):
-    sql = """SELECT wallet_id FROM l2ig_db WHERE line_id=%s"""
-    # try:
-    cur = conn.cursor()
-    # cur.execute("""INSERT INTO l2ig_db(line_id,wallet_id) VALUES ('test_line_id=====','======test_wallet_id');""")
-    cur.execute(sql,(line_id,))
-    rows = cur.fetchall()
-    cur.close()
-    print rows
-    return rows
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
 
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    sql = """SELECT wallet_id FROM l2ig_db WHERE line_id=%s"""
+    try:
+        cur = conn.cursor()
+        cur.execute(sql,(line_id,))
+        rows = cur.fetchall()
+        cur.close()
+        print rows
+        return rows
+
+    finally:
+        if conn is not None:
+            conn.close()
 
 def insert_test(line_id,wallet_id):
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
     sql = """INSERT INTO l2ig_db(line_id,wallet_id) VALUES (%s,%s);"""
-    # try:
-    cur = conn.cursor()
-    # cur.execute("""INSERT INTO l2ig_db(line_id,wallet_id) VALUES ('test_line_id=====','======test_wallet_id');""")
-    cur.execute(sql,(line_id,wallet_id,))
-    conn.commit()
-    cur.close()
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    try:
+        cur = conn.cursor()
+        cur.execute(sql,(line_id,wallet_id,))
+        conn.commit()
+        cur.close()
+    finally:
+        if conn is not None:
+            conn.close()
 
 def delete_by_line(line_id):
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
     sql = """DELETE FROM l2ig_db WHERE line_id=%s;"""
-    # try:
-    cur = conn.cursor()
-    # cur.execute("""INSERT INTO l2ig_db(line_id,wallet_id) VALUES ('test_line_id=====','======test_wallet_id');""")
-    cur.execute(sql,(line_id,))
-    conn.commit()
-    cur.close()
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
+    try:
+        cur = conn.cursor()
+        cur.execute(sql,(line_id,))
+        conn.commit()
+        cur.close()
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 if __name__ == '__main__':
