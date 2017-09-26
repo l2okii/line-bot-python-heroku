20116e03fd9ed35ebc10144c30f82576a2a2df59
l2okii <sfuangk@celestica.com>
2017-09-26 14:39:24 +0700

    add db function

--- db_adapter.py
+++ db_adapter.py
@@ -32,6 +32,20 @@ def select_test():
         if conn is not None:
             conn.close()
 
+def select_by_line(line_id):
+    sql = """SELECT wallet_id FROM l2ig_db WHERE line_id=%s"""
+    try:
+        cur = conn.cursor()
+        # cur.execute("""INSERT INTO l2ig_db(line_id,wallet_id) VALUES ('test_line_id=====','======test_wallet_id');""")
+        cur.execute(sql,(line_id,))
+        rows = cur.fetchall()
+        cur.close()
+        print rows
+        return rows
+
+    finally:
+        if conn is not None:
+            conn.close()
 
 def insert_test(line_id,wallet_id):
     sql = """INSERT INTO l2ig_db(line_id,wallet_id) VALUES (%s,%s);"""
@@ -45,6 +59,21 @@ def insert_test(line_id,wallet_id):
         if conn is not None:
             conn.close()
 
+def delete_by_line(line_id):
+    sql = """DELETE FROM l2ig_db WHERE line_id=%s;"""
+    try:
+        cur = conn.cursor()
+        # cur.execute("""INSERT INTO l2ig_db(line_id,wallet_id) VALUES ('test_line_id=====','======test_wallet_id');""")
+        cur.execute(sql,(line_id,))
+        conn.commit()
+        cur.close()
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 if __name__ == '__main__':
-    select_test()
-    # insert_test('1','2')
+    # select_test()
+    # insert_test('kakline1','kakwall2')
+    # select_by_line('kakline1')
+    # delete_by_line('kakline1')
