33bb394de5116a1582e675759cd58ef1bbff4527
l2okii <sfuangk@celestica.com>
2017-09-26 16:27:34 +0700

    add update db function and check duplicate regis

--- app.py
+++ app.py
@@ -165,10 +165,10 @@ def handle_text_message(event):
         ack_text = 'Please register first!!'
 
     elif text == 'hashing rate':
-        ack_text = get_data_now(1)
+        ack_text = get_data_now(wallet,1)
         # ack_text = 'hassssssh'
     elif text == 'all status':
-        ack_text = get_data_now(0)
+        ack_text = get_data_now(wallet,0)
         # ack_text = 'hwwwwww s'
     elif text == 'coin price':
         ack_text = 'kuy price'
