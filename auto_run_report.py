1ac7b8f361792be17a9bb0fade3749f155c46d0c
l2okii <sfuangk@celestica.com>
2017-09-27 16:51:14 +0700

    debuging

--- auto_run_report.py
+++ auto_run_report.py
@@ -27,6 +27,7 @@ def auto_report(line_id, wallet_id,time_interval=30,t_stop_flag=0):
         if current_thread().getName() == (line_id+'_thread'):
             t_stop.clear()
             print 'after stop thread = ', enumerate()
+        t_stop.clear()
         t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop), name=line_id+'_thread').start()
 
 
