25a7f8a7db29ea0803bb8056a5d82b62f4e92b1f
l2okii <sfuangk@celestica.com>
2017-09-27 15:17:13 +0700

    seperate function

--- data_getter.py
+++ data_getter.py
@@ -5,6 +5,38 @@ import json
 import db_adapter
 import coin_price
 
+algo_list = [
+'Scrypt'
+,'SHA256'
+,'ScryptNf'
+,'X11'
+,'X13'
+,'Keccak'
+,'X15'
+,'Nist5'
+,'NeoScrypt'
+,'Lyra2RE'
+,'WhirlpoolX'
+,'Qubit'
+,'Quark'
+,'Axiom'
+,'Lyra2REv2'
+,'ScryptJaneNf16'
+,'Blake256r8'
+,'Blake256r14'
+,'Blake256r8vnl'
+,'Hodl'
+,'DaggerHashimoto'
+,'Decred'
+,'CryptoNight'
+,'Lbry'
+,'Equihash'
+,'Pascal'
+,'X11Gost'
+,'Sia'
+,'Blake2s'
+,'Skunk'
+]
 
 api_link = 'https://api.nicehash.com/api?method=stats.provider&addr='
 
