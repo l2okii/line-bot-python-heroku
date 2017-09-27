4da0eefd21c8b0fa113fa8843ac81c64b4fc5308
l2okii <sfuangk@celestica.com>
2017-09-27 17:05:25 +0700

    test real report

--- auto_run_report.py
+++ auto_run_report.py
@@ -2,8 +2,8 @@ import os
 import requests
 from threading import *
 import time
-# import data_getter
-# import coin_price
+import data_getter
+import coin_price
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -54,37 +54,37 @@ def run(time_interval, line_id, wallet_id, t_stop):
     while (not t_stop.is_set()):
         print 'in thread - ', time_interval, '- name -', current_thread().getName(), '-- enumerate --', enumerate()
 
-        # price = coin_price.get_data()
-        # data = data_getter.get_data_now(wallet_id,0)
-        # if data == -1:
-        #     continue
-        # p_data = data_getter.process_data(data)
-        # if p_data == -1:
-        #     continue
-        #
-        # sending_text = '===================\n'
-        # sending_text += 'Net Balance : \n' + str(p_data[len(p_data)-1]) + ' BTC\n'
-        # sending_text += str(p_data[len(p_data)-1]*price['BTC']) + ' THB\n'
-        # sending_text += '===================\n'
-        #
-        # is_running = False
-        # # print 'len p data = ', len(p_data[:-1]) , 'p data = ', p_data
-        # for x in p_data[:-1]:
-        #     if get_short == 1 and float(x['speed'][:-3]) == 0:
-        #         continue
-        #     else:
-        #         is_running = True
-        #         sending_text += 'algo : ' + x['algo'] +'\n'
-        #         sending_text += 'speed : ' + x['speed'] + '\n'
-        #         sending_text += 'balance : ' + x['balance'] + '\n'
-        #         sending_text += '===================\n'
-        #
-        # if is_running == False:
-        #     sending_text += 'Miner Offline !!!\n'
-        #     sending_text += 'Please Check.\n'
-        #     sending_text += '===================\n'
-        #
-        # send_line_notify(sending_text, line_id)
+        price = coin_price.get_data()
+        data = data_getter.get_data_now(wallet_id,0)
+        if data == -1:
+            continue
+        p_data = data_getter.process_data(data)
+        if p_data == -1:
+            continue
+
+        sending_text = '===================\n'
+        sending_text += 'Net Balance : \n' + str(p_data[len(p_data)-1]) + ' BTC\n'
+        sending_text += str(p_data[len(p_data)-1]*price['BTC']) + ' THB\n'
+        sending_text += '===================\n'
+
+        is_running = False
+        # print 'len p data = ', len(p_data[:-1]) , 'p data = ', p_data
+        for x in p_data[:-1]:
+            if get_short == 1 and float(x['speed'][:-3]) == 0:
+                continue
+            else:
+                is_running = True
+                sending_text += 'algo : ' + x['algo'] +'\n'
+                sending_text += 'speed : ' + x['speed'] + '\n'
+                sending_text += 'balance : ' + x['balance'] + '\n'
+                sending_text += '===================\n'
+
+        if is_running == False:
+            sending_text += 'Miner Offline !!!\n'
+            sending_text += 'Please Check.\n'
+            sending_text += '===================\n'
+
+        send_line_notify(sending_text, line_id)
         time.sleep(time_interval)
 
 
