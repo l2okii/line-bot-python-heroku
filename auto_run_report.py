57569f076365275bb7d69673e9de4af357be1687
l2okii <sfuangk@celestica.com>
2017-09-27 15:14:12 +0700

    seperate function

--- auto_run_report.py
+++ auto_run_report.py
@@ -20,7 +20,7 @@ def auto_report(line_id, wallet_id,time_interval=30):
 
 
 def run(time_interval, line_id, wallet_id):
-    while true:
+    while True:
         price = coin_price.get_data()
         data = data_getter.get_data_now(wallet_id,0)
         if data == -1:
