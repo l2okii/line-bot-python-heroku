1a48e7f345a5cf948829d8a948a7ed8f1ec4cf2b
l2okii <sfuangk@celestica.com>
2017-09-28 11:02:23 +0700

    debug auto report

--- auto_run_report.py
+++ auto_run_report.py
@@ -45,6 +45,8 @@ def auto_report(line_id, wallet_id,time_interval=30,t_stop_flag=0):
             t_stop.clear()
         t_flag.is_run = 1
         t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop), name=line_id+'_thread').start()
+        aa = t.local()
+        aa.var = 0
         # print dir(t_flag), '2222222'
 
     # print '\n2222current thread = ', current_thread().getName(), ' -- all thread --' , enumerate(), 't_flag = ', t_flag.is_run, '=====', dir(t_flag)
@@ -55,7 +57,7 @@ def run(time_interval, line_id, wallet_id, t_stop):
     while (not t_stop.is_set()):
         print '==============================='
         print 'in thread - ', time_interval, '- name -', current_thread().getName(), '-- enumerate --', enumerate()
-        print dir(t_flag), 'thread name = ', current_thread().getName()
+        print dir(aa), 'thread name = ', current_thread().getName()
         print '==============================='
 
         #
