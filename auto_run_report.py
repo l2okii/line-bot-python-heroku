5dda74bbc42c0f7eaaa292451a662ed1f74b8992
l2okii <sfuangk@celestica.com>
2017-09-28 11:09:03 +0700

    debug auto report

--- auto_run_report.py
+++ auto_run_report.py
@@ -46,9 +46,6 @@ def auto_report(line_id, wallet_id,time_interval=30,t_stop_flag=0):
             t_stop.clear()
         t_flag.is_run = 1
         t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop), name=line_id+'_thread')
-        aa = t.local()
-        aa.var = 0
-        print dir(aa), aa.var
         t.start()
 
         # print dir(t_flag), '2222222'
@@ -56,47 +53,47 @@ def auto_report(line_id, wallet_id,time_interval=30,t_stop_flag=0):
     # print '\n2222current thread = ', current_thread().getName(), ' -- all thread --' , enumerate(), 't_flag = ', t_flag.is_run, '=====', dir(t_flag)
 
 
-def run(time_interval, line_id, wallet_id, t_stop):
-    get_short = 1
-    while (not t_stop.is_set()):
-        print '==============================='
-        print 'in thread - ', time_interval, '- name -', current_thread().getName(), '-- enumerate --', enumerate()
-        print dir(aa), 'thread name = ', current_thread().getName()
-        print '==============================='
+    def run(time_interval, line_id, wallet_id, t_stop):
+        get_short = 1
+        while (not t_stop.is_set()):
+            print '==============================='
+            print 'in thread - ', time_interval, '- name -', current_thread().getName(), '-- enumerate --', enumerate()
+            print dir(t_flag), 'thread name = ', current_thread().getName()
+            print '==============================='
 
-        #
-        # price = coin_price.get_data()
-        # data = data_getter.get_data_now(wallet_id,0)
-        # if data == -1:
-        #     continue
-        # p_data = data_getter.process_data(data)
-        # if p_data == -1:
-        #     continue
-        #
-        # sending_text = '===================\n'
-        # sending_text += 'Net Balance : \n' + str(p_data[len(p_data)-1]) + ' BTC\n'
-        # sending_text += str(p_data[len(p_data)-1]*price['BTC']) + ' THB\n'
-        # sending_text += '===================\n'
-        #
-        # is_running = False
-        # # print 'len p data = ', len(p_data[:-1]) , 'p data = ', p_data
-        # for x in p_data[:-1]:
-        #     if get_short == 1 and float(x['speed'][:-3]) == 0:
-        #         continue
-        #     else:
-        #         is_running = True
-        #         sending_text += 'algo : ' + x['algo'] +'\n'
-        #         sending_text += 'speed : ' + x['speed'] + '\n'
-        #         sending_text += 'balance : ' + x['balance'] + '\n'
-        #         sending_text += '===================\n'
-        #
-        # if is_running == False:
-        #     sending_text += 'Miner Offline !!!\n'
-        #     sending_text += 'Please Check.\n'
-        #     sending_text += '===================\n'
-        #
-        # send_line_notify(sending_text, line_id)
-        time.sleep(time_interval)
+            #
+            # price = coin_price.get_data()
+            # data = data_getter.get_data_now(wallet_id,0)
+            # if data == -1:
+            #     continue
+            # p_data = data_getter.process_data(data)
+            # if p_data == -1:
+            #     continue
+            #
+            # sending_text = '===================\n'
+            # sending_text += 'Net Balance : \n' + str(p_data[len(p_data)-1]) + ' BTC\n'
+            # sending_text += str(p_data[len(p_data)-1]*price['BTC']) + ' THB\n'
+            # sending_text += '===================\n'
+            #
+            # is_running = False
+            # # print 'len p data = ', len(p_data[:-1]) , 'p data = ', p_data
+            # for x in p_data[:-1]:
+            #     if get_short == 1 and float(x['speed'][:-3]) == 0:
+            #         continue
+            #     else:
+            #         is_running = True
+            #         sending_text += 'algo : ' + x['algo'] +'\n'
+            #         sending_text += 'speed : ' + x['speed'] + '\n'
+            #         sending_text += 'balance : ' + x['balance'] + '\n'
+            #         sending_text += '===================\n'
+            #
+            # if is_running == False:
+            #     sending_text += 'Miner Offline !!!\n'
+            #     sending_text += 'Please Check.\n'
+            #     sending_text += '===================\n'
+            #
+            # send_line_notify(sending_text, line_id)
+            time.sleep(time_interval)
 
 
 
