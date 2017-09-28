6a27cab91d7c699f13a039b32e27fb9f60fd3147
l2okii <sfuangk@celestica.com>
2017-09-28 10:45:26 +0700

    debug auto report

--- auto_run_report.py
+++ auto_run_report.py
@@ -37,12 +37,12 @@ def auto_report(line_id, wallet_id,time_interval=30,t_stop_flag=0):
         for x in enumerate():
             if x.getName() == (line_id+'_thread'):
                 t_stop.clear()
-                t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop, t_flag), name=line_id+'_thread').start()
                 t_flag.is_run = 1
+                t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop, t_flag), name=line_id+'_thread').start()
         if t_stop.is_set():
             t_stop.clear()
-        t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop, t_flag), name=line_id+'_thread').start()
         t_flag.is_run = 1
+        t = Thread(target=run, args=(time_interval, line_id, wallet_id, t_stop, ), name=line_id+'_thread').start()
 
 
     # print '\n2222current thread = ', current_thread().getName(), ' -- all thread --' , enumerate(), 't_flag = ', t_flag.is_run, '=====', dir(t_flag)
