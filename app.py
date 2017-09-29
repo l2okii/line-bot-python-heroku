1484e66ed690f0307f860ad7129a250ffba4dd11
l2okii <sfuangk@celestica.com>
2017-09-29 13:29:52 +0700

    debug

--- app.py
+++ app.py
@@ -68,11 +68,11 @@ def get_data_now(wallet_id,get_short=0):
     price = coin_price.get_data()
     data = data_getter.get_data_now(wallet_id)
     if data == -1:
-        print 'data = -1'
+        # print 'data = -1'
         return -1
     p_data = data_getter.process_data(data)
     if p_data == -1:
-        print 'p_data = -1'
+        # print 'p_data = -1'
         return -1
 
     sending_text = '===================\n'
@@ -84,8 +84,8 @@ def get_data_now(wallet_id,get_short=0):
     is_running = False
     for x in p_data[:-1]:
         if get_short == 1 and float(x['speed'][:-3]) == 0:
-            print 'get short return = -1'
-            return -1
+            # print 'get short return = -1'
+            continue
         else:
             is_running = True
             sending_text += 'algo : ' + x['algo'] +'\n'
